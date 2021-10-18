c55362854e9d9d3e04b5fae62786e661dc643859
Hugo Rosa <hugo2760@gmail.com>
2021-10-18 18:12:22 -0300

    Criação do front end

--- index.js
+++ index.js
@@ -3,18 +3,13 @@ const app = express();
 
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get("/:nome/:lang",(req,res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = true;
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do programador",
-        inscritos: 8000,
-        msg: exibirMsg
-    });
+app.get("/",(req, res) => {
+    res.render("index");
 });
 
-app.listen(8080,()=>{console.log("App rodando!");});
+app.get("/perguntar",(req, res) => {
+    res.render("perguntar");
+})
+app.listen(8000,()=>{console.log("App rodando!");});
